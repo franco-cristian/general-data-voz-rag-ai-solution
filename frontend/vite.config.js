0e38cae778da048145ee3987778060d7dd168892
Cristian Franco <cristian.franko.cf@gmail.com>
2024-12-20 15:50:34 +0000

    ajustes workflow

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  publicDir: 'public',  // Directorio donde se encuentra index.html
+  publicDir: 'public',
   build: {
-    outDir: 'dist',  // Directorio de salida
+    outDir: 'dist',
     rollupOptions: {
-      input: 'public/index.html'  // Asegúrate de que apunta correctamente a index.html
+      input: 'public/index.html'
     }
   }
 });
+
